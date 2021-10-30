69dec4b16484c8422362e235dd83aaab10ec1fa3
Mustafa Alkaya <info.mustafaalkaya@gmail.com>
2021-10-30 14:53:12 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{   
+    //IEntity implment eden class 
+    public interface IEntity
+    {
+    }
+}
