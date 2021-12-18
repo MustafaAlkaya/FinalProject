08be82bcd136cdc0bad8ba0fcaa12571253a6c7e
Mustafa Alkaya <info.mustafaalkaya@gmail.com>
2021-12-18 19:59:30 +0300

    18.12.2021 13.Ders Sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi geçersiz";
         public static string MaintenanceTime = "Sistem Bakımda";
         public static string ProductsListed = "Ürünler Listelendi";
+        public static string ProductCountOfCategoryError="Limiti aştınız";
+        public static string ProductNameAlreadyExists="Bu isimde başka bir ürün var";
+        public static string CategoryLimitExceded = "Kategori limiti aşıldı yeni ürün eklenemiyor";
     }
 }
